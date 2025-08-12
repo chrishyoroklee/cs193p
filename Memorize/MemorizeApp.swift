7665e569dc0c7f5408667d8735400f76bc616660
Chris Hyorok Lee <hl3838@columbia.edu>
2025-08-12 16:43:10 -0400

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by 이효록 on 8/12/25.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
