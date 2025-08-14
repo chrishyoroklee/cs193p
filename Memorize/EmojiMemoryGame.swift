11bf8c6da7736c4be2532600e2a6c66413663e41
Chris Hyorok Lee <hl3838@columbia.edu>
2025-08-14 10:43:50 -0400

    Lecture 3 Update

---
+++ Memorize/EmojiMemoryGame.swift
@@ -0,0 +1,12 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by 이효록 on 8/14/25.
+//
+
+import SwiftUI
+
+//class EmojiMemoryGame {
+//    var model: MemoryGame<String>
+//}
--- Memorize/MemorizeGame.swift
+++ Memorize/MemorizeGame.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct MemorizeGame<CardContent>  {
+    var cards: Array<Card>
+    
+    func choose(card: Card){
+        
+    }
+    
+    struct Card {
+        var isFaceUp: Bool
+        var isMatched: Bool
+        var content: CardContent
+    }
+    
+}
